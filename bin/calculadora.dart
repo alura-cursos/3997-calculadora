519f496c8c2264df7594a38659503f7bce4297d8
ikyrie <mah.c5r@gmail.com>
2024-07-18 18:29:46 -0300

    video4.3

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -41,11 +41,14 @@ void main() {
   }
 
   void getOperacao() {
-    print("Digite uma operação");
+    print("Digite uma operação ${operacoes.toString()}");
     entrada = stdin.readLineSync();
     if (entrada != null) {
       if (operacoes.contains(entrada)) {
         operacao = entrada!;
+      } else {
+        print("Operação inválida");
+        getOperacao();
       }
     }
   }
