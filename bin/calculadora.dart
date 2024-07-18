0d26f1d200f1334dc9fdd1db6ca997794c06b8a7
ikyrie <mah.c5r@gmail.com>
2024-07-18 18:26:57 -0300

    video3.4

--- bin/calculadora.dart
+++ bin/calculadora.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 void main() {
-  double numeroUm = double.parse(stdin.readLineSync()!);
-  double numeroDois = double.parse(stdin.readLineSync()!);
-  String operacao = stdin.readLineSync()!;
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
 
   void soma() {
     print(numeroUm + numeroDois);
@@ -21,20 +21,24 @@ void main() {
     print(numeroUm * numeroDois);
   }
 
-  if (operacao == "+") {
-    soma();
-  } else {
-    if (operacao == "-") {
-      subtracao();
-    } else {
-      if (operacao == "*") {
-        multiplicacao();
-      } else {
-        if (operacao == "/") {
-          divisao();
-        }
-      }
+  String? entrada = stdin.readLineSync();
+
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroUm = double.parse(entrada);
     }
+  }
+
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    if (entrada != "") {
+      numeroDois = double.parse(entrada);
+    }
+  }
+
+  entrada = stdin.readLineSync();
+  if (entrada != null) {
+    operacao = entrada;
   }
 
   switch (operacao) {
